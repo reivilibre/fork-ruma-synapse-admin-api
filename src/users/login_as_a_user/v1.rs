86589cd0a5a45ded4ca21f538250b79ca771866d
Olivier Wilkinson (reivilibre) <oliverw@matrix.org>
2023-03-31 16:01:27 +0100

    oldify API

--- src/users/login_as_a_user/v1.rs
+++ src/users/login_as_a_user/v1.rs
@@ -1,36 +1,33 @@
 //! [POST /_synapse/admin/v1/users/:user_id/login](https://matrix-org.github.io/synapse/latest/admin_api/user_admin_api.html#login-as-a-user)
 
-use ruma::{
-    api::{metadata, request, response, Metadata},
-    UserId,
-};
+use ruma::{api::ruma_api, UserId};
 
-const METADATA: Metadata = metadata! {
-    method: POST,
-    rate_limited: false,
-    authentication: AccessToken,
-    history: {
-        unstable => "/_synapse/admin/v1/users/:user_id/login",
+ruma_api! {
+    metadata: {
+        description: "login as a user endpoint",
+        method: POST,
+        name: "login_as_a_user",
+        unstable_path: "/_synapse/admin/v1/users/:user_id/login",
+        rate_limited: false,
+        authentication: AccessToken,
     }
-};
 
-#[request]
-pub struct Request<'a> {
-    /// User to log in as.
-    #[ruma_api(path)]
-    pub user_id: &'a UserId,
+    request: {
+        /// User to log in as.
+        #[ruma_api(path)]
+        pub user_id: &'a UserId,
 
-    /// Optional: Integer UNIX timestamp in milliseconds for when the access token should expire.
-    /// If not specified, the returned access token does not expire.
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    #[ruma_api(body)]
-    pub valid_until_ms: Option<u64>,
-}
+        /// Optional: Integer UNIX timestamp in milliseconds for when the access token should expire.
+        /// If not specified, the returned access token does not expire.
+        #[serde(default, skip_serializing_if = "Option::is_none")]
+        #[ruma_api(body)]
+        pub valid_until_ms: Option<u64>,
+    }
 
-#[response]
-pub struct Response {
-    /// An opaque access token string, that can be used to control the user.
-    pub access_token: String,
+    response: {
+        /// An opaque access token string, that can be used to control the user.
+        pub access_token: String,
+    }
 }
 
 impl<'a> Request<'a> {
